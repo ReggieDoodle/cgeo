911afe6ddcad4218309868041bec599ed194574b
Dave <dave.thiss@gmail.com>
2025-03-29 15:45:49 +0000

    Populate and read UI

--- main/src/main/java/cgeo/geocaching/ui/dialog/DavesCoordsDialog.java
+++ main/src/main/java/cgeo/geocaching/ui/dialog/DavesCoordsDialog.java
@@ -13,6 +13,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import org.apache.commons.lang3.StringUtils;
+
 import cgeo.geocaching.R;
 import cgeo.geocaching.location.Geopoint;
 import cgeo.geocaching.sensors.LocationDataProvider;
@@ -23,7 +25,10 @@ public class DavesCoordsDialog {
     private final Context context;
     private final DialogCallback callback;
 
+    Spinner spinner;
     private Button bLatitude, bLongitude;
+    private EditText longitudeDegree, longitudeMinutes, longitudeSeconds, longitudeFraction;
+    private EditText latitudeDegree, latitudeMinutes, latitudeSeconds, latitudeFraction;
     private Geopoint gp;
 
     public DavesCoordsDialog(Context context, DialogCallback callback) {
@@ -39,7 +44,7 @@ public class DavesCoordsDialog {
 
     public void show(Geopoint location) {
 
-        gp = location;
+        gp = new Geopoint(location.getLatitude(), location.getLongitude());
 
         LayoutInflater inflater = LayoutInflater.from(context);
         final View theView = inflater.inflate(R.layout.daves_coords_dialog, null);
@@ -51,7 +56,7 @@ public class DavesCoordsDialog {
         dialog.show();
 
         // Populate the spinner with options
-        Spinner spinner = theView.findViewById(R.id.dialogSpinner);
+        spinner = theView.findViewById(R.id.dialogSpinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, new String[]{"DD MM.MMM", "DD MM SS.SSS", "DD.DDDDD"});
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
@@ -80,6 +85,16 @@ public class DavesCoordsDialog {
         bLatitude = theView.findViewById(R.id.hemisphereLatitude);
         bLongitude = theView.findViewById(R.id.hemisphereLongitude);
 
+        latitudeDegree = theView.findViewById(R.id.editTextLatDegrees);
+        latitudeMinutes = theView.findViewById(R.id.editTextLatMinutes);
+        latitudeSeconds = theView.findViewById(R.id.editTextLatSeconds);
+        latitudeFraction = theView.findViewById(R.id.editTextLatFraction);
+
+        longitudeDegree = theView.findViewById(R.id.editTextLonDegrees);
+        longitudeMinutes = theView.findViewById(R.id.editTextLonMinutes);
+        longitudeSeconds = theView.findViewById(R.id.editTextLonSeconds);
+        longitudeFraction = theView.findViewById(R.id.editTextLonFraction);
+
         bLatitude.setOnClickListener(v -> {
             final CharSequence text = bLatitude.getText();
             if (text.equals("N"))
@@ -100,17 +115,46 @@ public class DavesCoordsDialog {
         Button button = theView.findViewById(R.id.commitButton);
 
         button.setOnClickListener(v -> {
-            String userInputLatitude = inputLatitude.getText().toString();
-            String userInputLongitude = inputLongitude.getText().toString();
 
-            if (userInputLatitude.isEmpty() || userInputLongitude.isEmpty()) {
+            String result = ReadGui();
+
+            if (result.isEmpty()) {
                 Toast.makeText(context, "Please enter something!", Toast.LENGTH_SHORT).show();
             } else {
                 // Invoke the callback to notify that the dialog is closed
-                callback.onDialogClosed(userInputLatitude + " " + userInputLongitude);
+                callback.onDialogClosed(result);
                 dialog.dismiss();
             }
         });
+    }
+
+    private String ReadGui(){
+
+        String lat = bLatitude.getText().toString();
+        String lon = bLongitude.getText().toString();
+
+        lat += String.valueOf(latitudeDegree.getText());
+        lon += String.valueOf(longitudeDegree.getText());
+
+        String currentFormat = spinner.getSelectedItem().toString();
+
+        switch (currentFormat) {
+            case "DD MM.MMM":
+                lat += " " + latitudeMinutes.getText() + "." + latitudeFraction.getText();
+                lon += " " + longitudeMinutes.getText() + "." + longitudeFraction.getText();
+                break;
+            case "DD MM SS.SSS":
+                lat += " " + latitudeMinutes.getText() + " " + latitudeSeconds.getText()+ "." + latitudeFraction.getText();
+                lon += " " + longitudeMinutes.getText() + " " + longitudeSeconds.getText()+ "." + longitudeFraction.getText();
+                break;
+            case "DDD.DDDDD":
+            default:
+                lat += "." + latitudeFraction.getText();
+                lon += "." + longitudeFraction.getText();
+                break;
+
+        }
+        return lat + " " + lon;
     }
 
     private void UpdateGui(View v, String selectedOption) {
@@ -125,36 +169,82 @@ public class DavesCoordsDialog {
 
         switch (selectedOption) {
             case "DD MM.MMM":
-                v.findViewById(R.id.EditTextLatMinutes).setVisibility(View.VISIBLE);
-                v.findViewById(R.id.EditTextLatSeconds).setVisibility(View.GONE);
-                v.findViewById(R.id.EditTextLatFraction).setVisibility(View.VISIBLE);
 
-                v.findViewById(R.id.EditTextLonMinutes).setVisibility(View.VISIBLE);
-                v.findViewById(R.id.EditTextLonSeconds).setVisibility(View.GONE);
-                v.findViewById(R.id.EditTextLonFraction).setVisibility(View.VISIBLE);
+                latitudeDegree.setVisibility(View.VISIBLE);
+                latitudeDegree.setText(addZeros(gp.getDecMinuteLatDeg(),2));
+
+                latitudeMinutes.setVisibility(View.VISIBLE);
+                latitudeMinutes.setText(addZeros(gp.getDecMinuteLatMin(),2));
+
+                latitudeSeconds.setVisibility(View.GONE);
+
+                latitudeFraction.setVisibility(View.VISIBLE);
+                latitudeFraction.setText(addZeros(gp.getDecMinuteLatMinFrac(),3));
+
+                longitudeDegree.setVisibility(View.VISIBLE);
+                longitudeDegree.setText(addZeros(gp.getDecMinuteLonDeg(),3));
+
+                longitudeMinutes.setVisibility(View.VISIBLE);
+                longitudeMinutes.setText(addZeros(gp.getDecMinuteLonMin(),2));
+
+                longitudeSeconds.setVisibility(View.GONE);
+
+                longitudeFraction.setVisibility(View.VISIBLE);
+                longitudeFraction.setText(addZeros(gp.getDecMinuteLonMinFrac(),3));
                 break;
 
             case "DD MM SS.SSS":
-                v.findViewById(R.id.EditTextLatMinutes).setVisibility(View.VISIBLE);
-                v.findViewById(R.id.EditTextLatSeconds).setVisibility(View.VISIBLE);
-                v.findViewById(R.id.EditTextLatFraction).setVisibility(View.VISIBLE);
+                latitudeDegree.setVisibility(View.VISIBLE);
+                latitudeDegree.setText(addZeros(gp.getDecDegreeLatDeg(), 2));
 
-                v.findViewById(R.id.EditTextLonMinutes).setVisibility(View.VISIBLE);
-                v.findViewById(R.id.EditTextLonSeconds).setVisibility(View.VISIBLE);
-                v.findViewById(R.id.EditTextLonFraction).setVisibility(View.VISIBLE);
+                latitudeMinutes.setVisibility(View.VISIBLE);
+                latitudeMinutes.setText(addZeros(gp.getDMSLatMin(),2));
+
+                latitudeSeconds.setVisibility(View.VISIBLE);
+                latitudeSeconds.setText(addZeros(gp.getDMSLatSec(), 2));
+
+                latitudeFraction.setVisibility(View.VISIBLE);
+                latitudeFraction.setText(addZeros(gp.getDMSLatSecFrac(),3));
+
+                longitudeDegree.setVisibility(View.VISIBLE);
+                longitudeDegree.setText(addZeros(gp.getDMSLonDeg(),3));
+
+                longitudeMinutes.setVisibility(View.VISIBLE);
+                longitudeMinutes.setText(addZeros(gp.getDMSLonMin(),2));
+
+                longitudeSeconds.setVisibility(View.VISIBLE);
+                longitudeSeconds.setText(addZeros(gp.getDMSLonSec(),2));
+
+                longitudeFraction.setVisibility(View.VISIBLE);
+                longitudeFraction.setText(addZeros(gp.getDMSLonSecFrac(),3));
                 break;
 
             case "DD.DDDDD":
             default:
-                v.findViewById(R.id.EditTextLatMinutes).setVisibility(View.GONE);
-                v.findViewById(R.id.EditTextLatSeconds).setVisibility(View.GONE);
-                v.findViewById(R.id.EditTextLatFraction).setVisibility(View.VISIBLE);
+                latitudeDegree.setVisibility(View.VISIBLE);
+                latitudeDegree.setText(addZeros(gp.getDecDegreeLatDeg(),2));
 
-                v.findViewById(R.id.EditTextLonMinutes).setVisibility(View.GONE);
-                v.findViewById(R.id.EditTextLonSeconds).setVisibility(View.GONE);
-                v.findViewById(R.id.EditTextLonFraction).setVisibility(View.VISIBLE);
+                latitudeMinutes.setVisibility(View.GONE);
+                latitudeSeconds.setVisibility(View.GONE);
+
+                latitudeFraction.setVisibility(View.VISIBLE);
+                latitudeFraction.setText(addZeros(gp.getDecDegreeLatDegFrac(),5));
+
+                longitudeDegree.setVisibility(View.VISIBLE);
+                longitudeDegree.setText(addZeros(gp.getDecDegreeLonDeg(),3));
+
+                longitudeMinutes.setVisibility(View.GONE);
+                longitudeSeconds.setVisibility(View.GONE);
+
+                longitudeFraction.setVisibility(View.VISIBLE);
+                longitudeFraction.setText(addZeros(gp.getDecDegreeLonDegFrac(),5));
                 break;
         }
+
+    }
+
+    private static String addZeros(final int value, final int len) {
+        return StringUtils.leftPad(Integer.toString(value), len, '0');
     }
 }
 
